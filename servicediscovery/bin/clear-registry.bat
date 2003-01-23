cf7b9d395a7ce361a20c20f166805acab02d2e31
lgoldsto <lgoldsto>
2003-01-23 21:28:11 +0000

    Cleaned up copyright notice.

--- servicediscovery/bin/clear-registry.bat
+++ servicediscovery/bin/clear-registry.bat
@@ -4,11 +4,11 @@ REM "<copyright>"
 REM " Copyright 2002-2003 BBNT Solutions, LLC"
 REM " under sponsorship of the Defense Advanced Research Projects Agency (DARPA)"
 REM " and the Defense Logistics Agency (DLA)."
-REM ""
+
 REM " This program is free software; you can redistribute it and/or modify"
 REM " it under the terms of the Cougaar Open Source License as published by"
 REM " DARPA on the Cougaar Open Source Website (www.cougaar.org)."
-REM ""
+
 REM " THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS"
 REM " PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR"
 REM " IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF"
@@ -21,7 +21,6 @@ REM " PERFORMANCE OF THE COUGAAR SOFTWARE."
 REM "</copyright>"
 
 REM This batch file configures and runs the DeleteRegistryEntries program.
-
 REM Make sure that COUGAAR_INSTALL_PATH is specified
 IF NOT "%COUGAAR_INSTALL_PATH%" == "" GOTO L_2
 
@@ -30,41 +29,31 @@ ECHO COUGAAR_INSTALL_PATH not set!
 GOTO L_END
 :L_2
 
-REM Some value MUST be assigned to these variables, either by command line args or 
+REM Some value MUST be assigned to these variables, either by command line args or
 REM by a direct assignment below.  The app contains no defaults.
 set USER_USERNAME=%1
 set USER_PASSWORD=%2
 
 REM Ensure USER_USERNAME and USER_PASSWORD are defined
 IF NOT "%USER_USERNAME%" == "" GOTO L_3
-
 ECHO Usage: clear-registry (username) (password)
 GOTO L_END
-
 :L_3
 
 IF NOT "%USER_PASSWORD%" == "" GOTO L_4
-
 ECHO Usage: clear-registry (username) (password)
 GOTO L_END
-
 :L_4
 
 REM User-configurable section:
-
 set UDDI_HOST=localhost
 set UDDI_PORT=8080
-set QUERY_URL=http://%UDDI_HOST%:%UDDI_PORT%/uddi/inquiry 
+set QUERY_URL=http://%UDDI_HOST%:%UDDI_PORT%/uddi/inquiry
 set PUBLISH_URL=http://%UDDI_HOST%:%UDDI_PORT%/uddi/publishing
 
 set UDDIPROPERTIES=-Dorg.cougaar.servicediscovery.registry.queryURL=%QUERY_URL% -Dorg.cougaar.servicediscovery.registry.publishURL=%PUBLISH_URL% -Dorg.cougaar.servicediscovery.registry.user.username=%USER_USERNAME% -Dorg.cougaar.servicediscovery.registry.user.password=%USER_PASSWORD%
-
 set UDDICLASSPATH=%COUGAAR_INSTALL_PATH%\lib\servicediscovery.jar;%COUGAAR_INSTALL_PATH%\sys\commons-logging.jar;%COUGAAR_INSTALL_PATH%\sys\castor-0.9.3.9-xml.jar;%COUGAAR_INSTALL_PATH%\sys\xerces.jar;%COUGAAR_INSTALL_PATH%\sys\mail.jar;%COUGAAR_INSTALL_PATH%\sys\dom4j.jar;%COUGAAR_INSTALL_PATH%\sys\activation.jar;%COUGAAR_INSTALL_PATH%\sys\uddi4j.jar;%COUGAAR_INSTALL_PATH%\sys\soap.jar
 
 REM The path to java.exe must be in the PATH environment variable
-
 java %UDDIPROPERTIES% -classpath %UDDICLASSPATH% org.cougaar.servicediscovery.util.UDDI4JDeleteRegistryEntries
-
 :L_END
-
-
--- servicediscovery/bin/clear-registry.sh
+++ servicediscovery/bin/clear-registry.sh
@@ -21,8 +21,7 @@
 # "</copyright>"
 
 # This batch file configures and runs the DeleteRegistryEntries program.
-
-# Some value MUST be assigned to these variables, either by command line args or 
+# Some value MUST be assigned to these variables, either by command line args or
 # by a direct assignment here.  The app contains no defaults.
 USER_USERNAME=$1
 USER_PASSWORD=$2
@@ -30,27 +29,19 @@ USER_PASSWORD=$2
 # Make sure that COUGAAR_INSTALL_PATH is specified
 if [ "$COUGAAR_INSTALL_PATH" = "" ]; then
   echo "COUGAAR_INSTALL_PATH not set!"
-
 # Ensure USER_USERID and USER_PASSWORD are defined
 elif [ "$USER_USERNAME" = "" -o "$USER_PASSWORD" = "" ]; then
   echo "Usage: clear-registry.sh (user username) (user password)"
-
 else
-
   # User-configurable section:
-
   UDDI_HOST="localhost"
   UDDI_PORT="8080"
   QUERY_URL="http://$UDDI_HOST:$UDDI_PORT/uddi/inquiry"
   PUBLISH_URL="http://$UDDI_HOST:$UDDI_PORT/uddi/publishing"
 
   UDDIPROPERTIES="-Dorg.cougaar.servicediscovery.registry.queryURL=$QUERY_URL -Dorg.cougaar.servicediscovery.registry.publishURL=$PUBLISH_URL -Dorg.cougaar.servicediscovery.registry.user.username=$USER_USERNAME -Dorg.cougaar.servicediscovery.registry.user.password=$USER_PASSWORD"
-
   UDDICLASSPATH="$COUGAAR_INSTALL_PATH/lib/servicediscovery.jar:$COUGAAR_INSTALL_PATH/sys/xerces.jar:$COUGAAR_INSTALL_PATH/sys/commons-logging.jar:$COUGAAR_INSTALL_PATH/sys/castor-0.9.3.9-xml.jar:$COUGAAR_INSTALL_PATH/sys/mail.jar:$COUGAAR_INSTALL_PATH/sys/dom4j.jar:$COUGAAR_INSTALL_PATH/sys/activation.jar:$COUGAAR_INSTALL_PATH/sys/uddi4j.jar:$COUGAAR_INSTALL_PATH/sys/soap.jar"
 
   # The path to java.exe must be in the PATH environment variable
-
   java $UDDIPROPERTIES -classpath $UDDICLASSPATH org.cougaar.servicediscovery.util.UDDI4JDeleteRegistryEntries
-
 fi
-
