b52a9a523c937f01f7c8152ed57796c8342cfe36
ngivler <ngivler>
2003-04-08 14:30:45 +0000

    Adding in new SDClient config.

--- servicediscovery/data/csmart/recipe/ServiceDiscoveryBootstrapping-export.sql
+++ servicediscovery/data/csmart/recipe/ServiceDiscoveryBootstrapping-export.sql
@@ -1,8 +1,8 @@
-# Mysql dump 8.14
+# MySQL dump 8.16
 #
 # Host: localhost    Database: tempcopy
 #--------------------------------------------------------
-# Server version	3.23.39-nt
+# Server version	3.23.42-nt
 
 #
 # Dumping data for table 'alib_component'
@@ -10,8 +10,8 @@
 
 LOCK TABLES alib_component WRITE;
 REPLACE INTO alib_component (COMPONENT_ALIB_ID, COMPONENT_NAME, COMPONENT_LIB_ID, COMPONENT_TYPE, CLONE_SET_ID) VALUES ('ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping','recipe|##RECIPE_CLASS##','recipe',0.000000000000000000000000000000);
-REPLACE INTO alib_component (COMPONENT_ALIB_ID, COMPONENT_NAME, COMPONENT_LIB_ID, COMPONENT_TYPE, CLONE_SET_ID) VALUES ('ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin',0.000000000000000000000000000000);
 REPLACE INTO alib_component (COMPONENT_ALIB_ID, COMPONENT_NAME, COMPONENT_LIB_ID, COMPONENT_TYPE, CLONE_SET_ID) VALUES ('ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','plugin|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','plugin',0.000000000000000000000000000000);
+REPLACE INTO alib_component (COMPONENT_ALIB_ID, COMPONENT_NAME, COMPONENT_LIB_ID, COMPONENT_TYPE, CLONE_SET_ID) VALUES ('ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin',0.000000000000000000000000000000);
 REPLACE INTO alib_component (COMPONENT_ALIB_ID, COMPONENT_NAME, COMPONENT_LIB_ID, COMPONENT_TYPE, CLONE_SET_ID) VALUES ('ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDProviderPlugin','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDProviderPlugin','plugin|org.cougaar.servicediscovery.plugin.SDProviderPlugin','plugin',0.000000000000000000000000000000);
 UNLOCK TABLES;
 
@@ -41,7 +41,7 @@ UNLOCK TABLES;
 #
 
 LOCK TABLES asb_assembly WRITE;
-INSERT INTO asb_assembly (ASSEMBLY_ID, ASSEMBLY_TYPE, DESCRIPTION) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','RCP','SDClient [1]');
+INSERT INTO asb_assembly (ASSEMBLY_ID, ASSEMBLY_TYPE, DESCRIPTION) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','RCP','ServiceDiscoveryBootstrapping');
 UNLOCK TABLES;
 
 #
@@ -49,12 +49,12 @@ UNLOCK TABLES;
 #
 
 LOCK TABLES asb_component_arg WRITE;
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','SparePartsProvider',6.000000000000000000000000000000);
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','SubsistenceSupplyProvider',5.000000000000000000000000000000);
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','AmmunitionProvider',4.000000000000000000000000000000);
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','PackagedPOLSupplyProvider',3.000000000000000000000000000000);
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','FuelSupplyProvider',2.000000000000000000000000000000);
-INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','StrategicTransportationProvider',1.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','AmmunitionProvider',4.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','FuelSupplyProvider',2.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','PackagedPOLSupplyProvider',3.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','SparePartsProvider',6.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','StrategicTransportationProvider',1.000000000000000000000000000000);
+INSERT INTO asb_component_arg (ASSEMBLY_ID, COMPONENT_ALIB_ID, ARGUMENT, ARGUMENT_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','SubsistenceSupplyProvider',5.000000000000000000000000000000);
 UNLOCK TABLES;
 
 #
@@ -62,9 +62,9 @@ UNLOCK TABLES;
 #
 
 LOCK TABLES asb_component_hierarchy WRITE;
-INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','ServiceDiscoveryBootstrapping','COMPONENT',0.000000000000000000000000000000);
-INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','ServiceDiscoveryBootstrapping','COMPONENT',1.000000000000000000000000000000);
-INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDProviderPlugin','ServiceDiscoveryBootstrapping','COMPONENT',2.000000000000000000000000000000);
+INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','ServiceDiscoveryBootstrapping','COMPONENT',1.000000000000000000000000000000);
+INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDClientPlugin','ServiceDiscoveryBootstrapping','COMPONENT',0.000000000000000000000000000000);
+INSERT INTO asb_component_hierarchy (ASSEMBLY_ID, COMPONENT_ALIB_ID, PARENT_COMPONENT_ALIB_ID, PRIORITY, INSERTION_ORDER) VALUES ('RCP-0001-ServiceDiscoveryBootstrapping','ServiceDiscoveryBootstrapping|org.cougaar.servicediscovery.plugin.SDProviderPlugin','ServiceDiscoveryBootstrapping','COMPONENT',2.000000000000000000000000000000);
 UNLOCK TABLES;
 
 #
@@ -108,8 +108,8 @@ UNLOCK TABLES;
 
 LOCK TABLES lib_component WRITE;
 REPLACE INTO lib_component (COMPONENT_LIB_ID, COMPONENT_TYPE, COMPONENT_CLASS, INSERTION_POINT, DESCRIPTION) VALUES ('recipe|##RECIPE_CLASS##','recipe','##RECIPE_CLASS##','recipe','Added recipe');
-REPLACE INTO lib_component (COMPONENT_LIB_ID, COMPONENT_TYPE, COMPONENT_CLASS, INSERTION_POINT, DESCRIPTION) VALUES ('plugin|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin','org.cougaar.servicediscovery.plugin.SDClientPlugin','Node.AgentManager.Agent.PluginManager.Plugin','Added plugin');
 REPLACE INTO lib_component (COMPONENT_LIB_ID, COMPONENT_TYPE, COMPONENT_CLASS, INSERTION_POINT, DESCRIPTION) VALUES ('plugin|org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','plugin','org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin','Node.AgentManager.Agent.PluginManager.Plugin','Added plugin');
+REPLACE INTO lib_component (COMPONENT_LIB_ID, COMPONENT_TYPE, COMPONENT_CLASS, INSERTION_POINT, DESCRIPTION) VALUES ('plugin|org.cougaar.servicediscovery.plugin.SDClientPlugin','plugin','org.cougaar.servicediscovery.plugin.SDClientPlugin','Node.AgentManager.Agent.PluginManager.Plugin','Added plugin');
 REPLACE INTO lib_component (COMPONENT_LIB_ID, COMPONENT_TYPE, COMPONENT_CLASS, INSERTION_POINT, DESCRIPTION) VALUES ('plugin|org.cougaar.servicediscovery.plugin.SDProviderPlugin','plugin','org.cougaar.servicediscovery.plugin.SDProviderPlugin','Node.AgentManager.Agent.PluginManager.Plugin','Added plugin');
 UNLOCK TABLES;
 
@@ -126,11 +126,9 @@ UNLOCK TABLES;
 #
 
 LOCK TABLES lib_mod_recipe_arg WRITE;
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','$$CP=org.cougaar.servicediscovery.plugin.MatchmakerStubPlugin-1',4.000000000000000000000000000000,'recipeQueryAllAgents');
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','$$CP=org.cougaar.servicediscovery.plugin.SDClientPlugin-0',2.000000000000000000000000000000,'recipeQueryAllAgents');
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','$$CP=org.cougaar.servicediscovery.plugin.SDProviderPlugin-2',3.000000000000000000000000000000,'recipeQueryAllAgents');
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','Assembly Id',0.000000000000000000000000000000,'RCP-0001-ServiceDiscoveryBootstrapping-ServiceDisc');
-REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','Target Component Selection Query',1.000000000000000000000000000000,'recipeQuerySelectNothing');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','$$CP=org.cougaar.servicediscovery.plugin.SDClientPlugin-0',2.000000000000000000000000000000,'recipeQueryAllClientAgents');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','Assembly Id',0.000000000000000000000000000000,'RCP-0001-ServiceDiscoveryBootstrapping');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0001ServiceDiscoveryBootstrapping','Target Component Selection Query',1.000000000000000000000000000000,'recipeQueryAllAgents');
 UNLOCK TABLES;
 
 #
