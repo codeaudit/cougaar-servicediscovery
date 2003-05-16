68e475ddeaa03521c856e54f03227863d56ab188
ngivler <ngivler>
2003-05-16 17:30:07 +0000

    Back out staged oplan support. Belongs on B_OPLAN branch.

--- servicediscovery/src/org/cougaar/servicediscovery/plugin/SDClientPlugin.java
+++ servicediscovery/src/org/cougaar/servicediscovery/plugin/SDClientPlugin.java
@@ -26,37 +26,33 @@ import org.cougaar.core.blackboard.IncrementalSubscription;
 import org.cougaar.core.service.LoggingService;
 import org.cougaar.core.mts.MessageAddress;
 
-import org.cougaar.planning.ldm.plan.AllocationResult;
 import org.cougaar.planning.ldm.plan.AspectType;
-import org.cougaar.planning.ldm.plan.AspectValue;
-import org.cougaar.planning.ldm.plan.Disposition;
-import org.cougaar.planning.ldm.plan.PlanElement;
 import org.cougaar.planning.ldm.plan.Preference;
 import org.cougaar.planning.ldm.plan.Role;
 import org.cougaar.planning.ldm.plan.ScoringFunction;
-import org.cougaar.planning.ldm.plan.Task;
 import org.cougaar.planning.ldm.plan.TimeAspectValue;
-import org.cougaar.planning.ldm.plan.Verb;
+import org.cougaar.planning.ldm.plan.AspectValue;
 import org.cougaar.planning.plugin.legacy.SimplePlugin;
-import org.cougaar.planning.plugin.util.PluginHelper;
 import org.cougaar.util.UnaryPredicate;
 
-
-import org.cougaar.glm.ldm.Constants;
 import org.cougaar.glm.ldm.oplan.Oplan;
+import org.cougaar.glm.ldm.asset.MilitaryOrgPG;
 import org.cougaar.glm.ldm.asset.Organization;
 
+import org.cougaar.servicediscovery.Constants;
 import org.cougaar.servicediscovery.SDDomain;
 import org.cougaar.servicediscovery.SDFactory;
+
 import org.cougaar.servicediscovery.description.MMRoleQuery;
+import org.cougaar.servicediscovery.description.LineageList;
 import org.cougaar.servicediscovery.description.ScoredServiceDescription;
 import org.cougaar.servicediscovery.description.ServiceClassification;
 import org.cougaar.servicediscovery.description.ServiceDescription;
 import org.cougaar.servicediscovery.description.ServiceRequest;
-import org.cougaar.servicediscovery.util.UDDIConstants;
+
 import org.cougaar.servicediscovery.transaction.MMQueryRequest;
 import org.cougaar.servicediscovery.transaction.ServiceContractRelay;
-
+import org.cougaar.servicediscovery.util.UDDIConstants;
 
 
 import java.util.ArrayList;
@@ -72,14 +68,22 @@ public class SDClientPlugin extends SimplePlugin {
   private IncrementalSubscription myOplanSubscription;
   private IncrementalSubscription mySelfOrgSubscription;
   private IncrementalSubscription myMMRequestSubscription;
+  private IncrementalSubscription lineage;
   private IncrementalSubscription myServiceContractRelaySubscription;
-  private IncrementalSubscription myFindProvidersTaskSubscription;
 
   private LoggingService myLoggingService;
 
   private SDFactory mySDFactory;
 
-  private boolean myFindingProviders = false;
+  private UnaryPredicate lineageListPredicate = new UnaryPredicate() {
+    public boolean execute(Object o) {
+      if(o instanceof LineageList){
+        if(((LineageList)o).getType() == LineageList.COMMAND){
+          return true;
+        }
+      }
+      return false;
+    }};
 
   private UnaryPredicate myOplanPred = new UnaryPredicate() {
     public boolean execute(Object o) {
@@ -116,13 +120,6 @@ public class SDClientPlugin extends SimplePlugin {
     }
   };
 
-  private UnaryPredicate myFindProvidersTaskPred = new UnaryPredicate() {
-    public boolean execute(Object o) {
-      return ((o instanceof Task) &&
-	      (((Task) o).getVerb().equals(Constants.Verb.FindProviders)));
-    }
-  };
-
   private static Calendar myCalendar = Calendar.getInstance();
 
   private static long DEFAULT_START_TIME = -1;
@@ -143,50 +140,84 @@ public class SDClientPlugin extends SimplePlugin {
     mySelfOrgSubscription = (IncrementalSubscription)subscribe(mySelfOrgPred);
     myMMRequestSubscription = (IncrementalSubscription)subscribe(myMMRequestPred);
     myServiceContractRelaySubscription = (IncrementalSubscription)subscribe(myServiceContractRelayPred);
-    myFindProvidersTaskSubscription = (IncrementalSubscription)subscribe(myFindProvidersTaskPred);
 
     myLoggingService =
       (LoggingService) getBindingSite().getServiceBroker().getService(this, LoggingService.class, null);
 
     mySDFactory = (SDFactory) getFactory(SDDomain.SD_NAME);
 
-    if (didRehydrate()) {
-      myFindingProviders = false;
+    // subscribe to lineage
+    lineage = (IncrementalSubscription)getBlackboardService().subscribe(lineageListPredicate);
 
-      if (needToFindProviders()) {
-	myFindingProviders = true;
-	Collection params = getDelegate().getParameters();    
-	
-	for (Iterator iterator = params.iterator();
-	     iterator.hasNext();) {
-	  Role role = Role.getRole((String) iterator.next());
-	  boolean foundContract = checkProvider(role);
-
-	  if (!foundContract) {
-	    queryServices(role);
-	  }
-	}
-      }
-    }
   }
 
   public void execute() {
-    if ((myOplanSubscription.hasChanged() &&
-	(needToFindProviders()))) {
-      findProviders();
+    if (myOplanSubscription.getAddedCollection().size() > 0) {
+      Collection params = getDelegate().getParameters();
+
+      for (Iterator iterator = params.iterator();
+           iterator.hasNext();) {
+        Role role = Role.getRole((String) iterator.next());
+        queryServices(role);
+      }
     }
 
     if (myMMRequestSubscription.hasChanged()) {
-      generateServiceRequests(myMMRequestSubscription.getChangedCollection());
+      for (Iterator iterator =
+        myMMRequestSubscription.getChangedCollection().iterator();
+           iterator.hasNext();) {
+        MMQueryRequest mmRequest = (MMQueryRequest) iterator.next();
+
+        if (myLoggingService.isDebugEnabled()) {
+          myLoggingService.debug("execute: MMQueryRequest has changed." +
+                                 mmRequest);
+        }
+        if (mmRequest.getResult() != null) {
+          Collection services = mmRequest.getResult();
+          myLoggingService.debug("execute: services size: " + services.size());
+
+          // print all results
+	  if (myLoggingService.isDebugEnabled()) {
+
+	    myLoggingService.debug("Results for query "+mmRequest.getQuery().toString());
+	    for (Iterator serviceIterator = services.iterator();
+		 serviceIterator.hasNext();) {
+	      ScoredServiceDescription serviceDescription =
+		(ScoredServiceDescription) serviceIterator.next();
+	      myLoggingService.debug("Score " + serviceDescription.getScore());
+	      myLoggingService.debug("Provider name " + serviceDescription.getProviderName());
+	      myLoggingService.debug("*********");
+	    }
+	  }
+
+          for (Iterator serviceIterator = services.iterator();
+               serviceIterator.hasNext();) {
+            ScoredServiceDescription serviceDescription =
+              (ScoredServiceDescription) serviceIterator.next();
+	    if (myLoggingService.isDebugEnabled()) {
+	      myLoggingService.debug(getAgentIdentifier() +
+				     " execute: - provider: " +
+				     serviceDescription.getProviderName() +
+				     " score: " + serviceDescription.getScore());
+	    }
+
+            requestServiceContract(serviceDescription);
+
+            // only want one contract
+            break;
+          }
+
+          // Done with the query so clean up
+          // publishRemove(mmRequest);
+        }
+      }
     }
-    
+
     //if your relays have changed, check for revokes
     if(myServiceContractRelaySubscription.hasChanged()) {
-      Collection changedRelays = 
-	myServiceContractRelaySubscription.getChangedCollection();
-      for (Iterator iterator = changedRelays.iterator();
-	   iterator.hasNext();) {
-        ServiceContractRelay relay = (ServiceContractRelay)iterator.next();
+      Iterator relays = myServiceContractRelaySubscription.getChangedCollection().iterator();
+      while(relays.hasNext()) {
+        ServiceContractRelay relay = (ServiceContractRelay)relays.next();
 
         //actually, you only want to take action if you are the client agent
 	// (not if you are the provider)
@@ -194,15 +225,9 @@ public class SDClientPlugin extends SimplePlugin {
         if ((relay.getServiceContract().isRevoked()) &&
 	    (relay.getClient().equals(getSelfOrg()))) {
 	  //if your service contract got revoked, do a new service query
-	  queryServices(relay.getServiceContract().getServiceRole());
-	  //publishRemove(relay);
+	  queryServices(relay.getServiceRequest().getServiceRole());
 	}
       }
-    }
-
-    if (myFindProvidersTaskSubscription.hasChanged()) {
-      Collection adds = myFindProvidersTaskSubscription.getAddedCollection();
-      updateFindProvidersTaskDispositions(adds);
     }
   }
 
@@ -240,18 +265,25 @@ public class SDClientPlugin extends SimplePlugin {
   }
 
   protected void queryServices(Role role) {
+
+    String clientName=null;
+    Iterator lineageIterator = lineage.iterator();
+    if(lineageIterator.hasNext()){
+      LineageList lineageCollection = (LineageList)lineageIterator.next();
+      clientName = lineageCollection.getLeaf();
+    }
+
     MMQueryRequest mmRequest;
 
     if (myLoggingService.isDebugEnabled()) {
-      myLoggingService.debug(getAgentIdentifier() + 
-			     " asking MatchMaker for : " + role);
+      myLoggingService.debug(clientName + " asking MatchMaker for : " + role);
     }
       mmRequest =
         mySDFactory.newMMQueryRequest(new MMRoleQuery(role, null));
     publishAdd(mmRequest);
   }
 
-  protected Collection getDefaultPreferences(long startTime, long endTime) {
+  Collection getDefaultPreferences(long startTime, long endTime) {
     ArrayList preferences = new ArrayList(2);
 
     AspectValue startTAV = TimeAspectValue.create(AspectType.START_TIME, startTime);
@@ -272,141 +304,6 @@ public class SDClientPlugin extends SimplePlugin {
     return preferences;
   }
 
-  private boolean needToFindProviders() {
-    if (myFindingProviders == true) {
-      return false;
-    }
-
-    Oplan oplan = null;
-    for (Iterator iterator = myOplanSubscription.iterator();
-	 iterator.hasNext();) {
-      oplan = (Oplan) iterator.next();
-      break;
-    }
-    
-    return ((oplan != null) &&
-	    (oplan.isActive()));
-  }
-
-  protected void findProviders() {
-    myFindingProviders = true;
-    Collection params = getDelegate().getParameters();
-    
-    for (Iterator iterator = params.iterator();
-	 iterator.hasNext();) {
-      Role role = Role.getRole((String) iterator.next());
-      queryServices(role);
-    }
-  }
-    
-  protected void generateServiceRequests(Collection mmRequests) {
-    for (Iterator iterator = mmRequests.iterator();
-	 iterator.hasNext();) {
-      MMQueryRequest mmRequest = (MMQueryRequest) iterator.next();
-      
-      if (myLoggingService.isDebugEnabled()) {
-	myLoggingService.debug("execute: MMQueryRequest has changed." +
-			       mmRequest);
-      }
-      if (mmRequest.getResult() != null) {
-	Collection services = mmRequest.getResult();
-	
-	if (myLoggingService.isDebugEnabled()) {
-	  
-	  myLoggingService.debug("Results for query " + 
-				 mmRequest.getQuery().toString());
-	  myLoggingService.debug("number of providers: " + 
-				 services.size());
-	}
-	
-	for (Iterator serviceIterator = services.iterator();
-	     serviceIterator.hasNext();) {
-	  ScoredServiceDescription serviceDescription =
-	    (ScoredServiceDescription) serviceIterator.next();
-	  if (myLoggingService.isDebugEnabled()) {
-	    myLoggingService.debug(getAgentIdentifier() +
-				   " execute: - provider: " +
-				   serviceDescription.getProviderName() +
-				   " score: " + serviceDescription.getScore());
-	  }
-	  
-	  requestServiceContract(serviceDescription);
-	  
-	  // only want one contract
-	  break;
-	}
-	
-	// Done with the query so clean up
-	// publishRemove(mmRequest);
-      }
-    }
-  }
-
-  protected boolean checkProvider(Role role) {
-    boolean foundContract = false;
-    
-    for (Iterator relayIterator = myServiceContractRelaySubscription.iterator();
-	 relayIterator.hasNext();) {
-      ServiceContractRelay relay = 
-	(ServiceContractRelay) relayIterator.next();
-      if (!relay.getServiceContract().isRevoked() &&
-	  relay.getServiceContract().getServiceRole().equals(role)) {
-	foundContract = true;
-	break;
-      }
-    }
-
-    return foundContract;
-  }
-
-  private void updateFindProvidersTaskDispositions(Collection findProvidersTasks) {
-    Oplan oplan = null;
-    for (Iterator iterator = myOplanSubscription.iterator();
-	 iterator.hasNext();) {
-      oplan = (Oplan) iterator.next();
-      break;
-    }
-
-
-    double conf = 1.0;
-
-    if ((oplan != null) &&
-	(oplan.isActive())) {
-      // Look to see if we've got all our contracts
-      
-      Collection params = getDelegate().getParameters();    
-      for (Iterator iterator = params.iterator();
-	   iterator.hasNext();) {
-	Role role = Role.getRole((String) iterator.next());
-	boolean foundContract = checkProvider(role);
-	
-	if (!foundContract) {
-	  conf = 0.0;
-	  break;
-	}
-      }
-    }
-
-    for (Iterator iterator = findProvidersTasks.iterator();
-	 iterator.hasNext();) {
-      Task task = (Task) iterator.next();
-      PlanElement pe = task.getPlanElement();
-
-      AllocationResult estResult = 
-	PluginHelper.createEstimatedAllocationResult(task, 
-						     theLDMF, 
-						     conf, 
-						     true);
-      if (pe == null) {
-	Disposition disposition = 
-	  theLDMF.createDisposition(task.getPlan(), task, estResult);
-	publishAdd(disposition);
-      } else {
-	pe.setEstimatedResult(estResult);
-	publishChange(pe);
-      }
-    }
-  }
 }
 
 
