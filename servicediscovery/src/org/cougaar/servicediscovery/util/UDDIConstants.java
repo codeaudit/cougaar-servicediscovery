229acb31d25f426028493226fead1e38ba258f86
ngivler <ngivler>
2004-11-01 19:44:23 +0000

    Removed unused UUID definitions.

--- servicediscovery/src/org/cougaar/servicediscovery/util/UDDIConstants.java
+++ servicediscovery/src/org/cougaar/servicediscovery/util/UDDIConstants.java
@@ -33,15 +33,9 @@ package org.cougaar.servicediscovery.util;
 public interface UDDIConstants {
 
   public final static String NAICS_TAXONOMY = "ntis-gov:naics:1997";
-  public final static String NAICS_TAXONOMY_UUID = "uuid:c0b9fe13-179f-413d-8a5b-5004db8e5bb2";
   public final static String MILITARY_SERVICE_SCHEME = "MilitaryServiceScheme";
-  public final static String MILITARY_SERVICE_SCHEME_UUID = "uuid:f0b01564-b8f0-b015-dad5-b49598339719";
   public final static String MILITARY_ECHELON_SCHEME = "MilitaryEchelonScheme";
-  public final static String MILITARY_ECHELON_SCHEME_UUID = "uuid:f0f45111-5af0-f451-8a06-0d5979065559";
   public final static String ORGANIZATION_TYPES = "OrganizationTypes";
-  public final static String ORGANIZATION_TYPES_UUID = "uuid:c71f3d00-fb35-11d6-8c6a-b8a03c50a862";
   public final static String SOURCING_CAPABILITY_SCHEME = "SourcingCapabilityScheme";
-  public final static String SOURCING_CAPABILITY_SCHEME_UUID = "uuid:df66b570-fc28-11d6-bc4e-b8a03c50a862";
   public final static String SUPPORT_COMMAND_ASSIGNMENT = "SupportCommandAssignment";
-  public final static String SUPPORT_COMMAND_ASSIGNMENTI_UUID = "uuid:059c66d0-f1bc-11d6-a353-b8a03c50a862";
 }
